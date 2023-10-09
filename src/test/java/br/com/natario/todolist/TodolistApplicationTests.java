b8b32dc6d5633c2d90515132bebcb296122b0047
Vitor Natario <natariovitor@gmail.com>
2023-10-09 11:12:23 -0300

    intial commit

---
+++ src/test/java/br/com/natario/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.natario.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
