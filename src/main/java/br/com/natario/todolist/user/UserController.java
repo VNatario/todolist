cbeaa36ccb4ef9cbfb28bac815846f3fb4f67459
Vitor Natario <natariovitor@gmail.com>
2023-10-11 15:23:06 -0300

    feat: use Data from lombok for getter and setters

--- src/main/java/br/com/natario/todolist/user/UserController.java
+++ src/main/java/br/com/natario/todolist/user/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-  
+
   @PostMapping("/")
   public void create(@RequestBody UserModel userModel) {
     System.out.println(userModel.getUsername());
